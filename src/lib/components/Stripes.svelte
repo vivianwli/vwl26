6a982e8633920924c1a3225e00b03be484675567
vivianwli <vivieli77@gmail.com>
2026-04-27 08:44:13 -0400

    stripes update

--- src/lib/components/Stripes.svelte
+++ src/lib/components/Stripes.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 
-  // generate wobbly path
+  type stripe = {
+    x: number;
+    d: string;
+    interval: NodeJS.Timeout | null;
+  };
+
+  // function: generate wobbly path
   const wobblePath = (x: number, height: number = 2000) => {
     const points = [];
     const steps = 200;
@@ -12,12 +18,14 @@
     return `M${points.join(' L')}`;
   };
 
+  // function: continuously regenerate path for jitter effect
   const startJitter = (stripe: stripe) => {
     stripe.interval = setInterval(() => {
      stripe.d = wobblePath(stripe.x);
     }, 100);
   };
 
+  // function: stop jitter and reset path
   const stopJitter = (stripe: stripe) => {
     if (stripe.interval) {
       clearInterval(stripe.interval);
@@ -26,12 +34,7 @@
     stripe.d = wobblePath(stripe.x);
   };
 
-  type stripe = {
-    x: number;
-    d: string;
-    interval: NodeJS.Timeout | null;
-  };
-
+  // initialize stripe paths
   let stripes = $state(
     Array.from({ length: 200 }, (_, i) => ({
       x: i * 10,
@@ -43,21 +46,20 @@
 
 <svg class="fixed inset-0 w-full h-full" xmlns="http://www.w3.org/2000/svg">
   {#each stripes as stripe (stripe.x)}
-    <path
-        d={stripe.d}
-        class="stroke-transparent stroke-10 fill-none pointer-events-auto"
-        role="presentation"
-        onclick={() => console.log(`Clicked stripe at x=${stripe.x}`)}
-        onmouseenter={() => startJitter(stripe)}
-        onmouseleave={() => stopJitter(stripe)}
-      />
-    <path
-      d={stripe.d}
-      class="stroke-primary/10 stroke-3 fill-none hover:stroke-primary/20 transition-colors duration-300"
+    <g 
+      class="group" 
       role="presentation"
-      onclick={() => console.log(`Clicked stripe at x=${stripe.x}`)}
       onmouseenter={() => startJitter(stripe)}
       onmouseleave={() => stopJitter(stripe)}
-     />
+    >
+      <path
+          d={stripe.d}
+          class="stroke-transparent stroke-10 fill-none"
+        />
+      <path
+        d={stripe.d}
+        class="stroke-primary/10 stroke-3 fill-none group-hover:stroke-primary/20 transition-colors duration-300"
+      />
+    </g>
   {/each}
 </svg>
